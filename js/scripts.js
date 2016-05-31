f9529953fb7d64d9c719aa0a725b75417ff22781
Olivia Hinton and Sam Park <olivia@>
2016-05-31 11:30:05 -0700

    add toggle function to show show happy calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
+$(function() {
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
+  });
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+  var add = function(number1, number2) {
+    return number1 + number2;
+  };
 
-alert(add(number1, number2));
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+
+  alert(divide(number1, number2));
+
+
+
+});
